bfeba9c882a34218a79b97dce760a568f00033ed
Ventavis <oblitusavis@gmail.com>
2025-05-11 00:22:40 +0400

    start

---
+++ android/src/main/java/end/team/center/android/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package end.team.center.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import end.team.center.Center;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        initialize(new Center(), configuration);
+    }
+}
