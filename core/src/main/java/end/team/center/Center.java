5c17310646d64123fdb244a510b810b6a19284b8
Ventavis <oblitusavis@gmail.com>
2025-05-21 02:39:18 +0400

    sad

--- core/src/main/java/end/team/center/Center.java
+++ core/src/main/java/end/team/center/Center.java
@@ -12,7 +12,7 @@ public class Center extends Game {
     public void create() {
         // Создание спрайт-объекта, который будет использоваться для отрисовки
         batch = new SpriteBatch();
-
+        setScreen(new MainMenuScreen());
 
     }
 
