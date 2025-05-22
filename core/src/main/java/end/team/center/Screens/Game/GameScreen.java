ba97e1005ba716bbdfbb9fcd17c29cc281277ff4
EliseyZaf <elisejbaranov46@gmail.com>
2025-05-22 21:06:23 +0400

    fg

--- core/src/main/java/end/team/center/Screens/Game/GameScreen.java
+++ core/src/main/java/end/team/center/Screens/Game/GameScreen.java
@@ -5,11 +5,15 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
+
+import end.team.center.GameCore.Objects.Hero;
+import end.team.center.Screens.Game.TouchpadClass;
 
 public class GameScreen implements Screen {
 
@@ -37,14 +41,14 @@ public class GameScreen implements Screen {
 
         // Создаём героя с нужными параметрами и добавляем на сцену
         hero = new Hero(
-            Gdx.graphics.getWidth() / 2f - 70,
-            Gdx.graphics.getHeight() / 2f - 80,
+            new Texture("UI/GameUI/Hero/heroRight.png"),
+            new Texture("UI/GameUI/Hero/heroLeft.png"),
+            new Vector2(Gdx.graphics.getWidth() / 2f - 70, Gdx.graphics.getHeight() / 2f - 80),
             140, 160,
             100,
-            500,
-            "UI/GameUI/Hero/heroRight.png",
-            "UI/GameUI/Hero/heroLeft.png"
-
+            5,
+            0,
+            500
         );
         stage.addActor(hero);
     }
@@ -67,7 +71,6 @@ public class GameScreen implements Screen {
 
 
         hero.move(moveX, moveY, delta);
-        hero.setPosition(hero.heroX, hero.heroY);
 
         stage.draw();
     }
