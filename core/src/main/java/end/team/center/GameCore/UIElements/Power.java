999ca9c0cf873e849c03c41fb5ff274bf9615a5e
EliseyZaf <elisejbaranov46@gmail.com>
2025-05-26 15:52:50 +0400

    add texture

--- core/src/main/java/end/team/center/GameCore/UIElements/Power.java
+++ core/src/main/java/end/team/center/GameCore/UIElements/Power.java
@@ -2,10 +2,12 @@ package end.team.center.GameCore.UIElements;
 
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 
 public class Power extends ImageButton {
-    public Power(Skin skin) {
-        super(skin);
+
+    public Power(Drawable imageUp) {
+        super(imageUp);
     }
 
     public void effect() {
