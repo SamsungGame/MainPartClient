3fbaa30f4d8c3a26c82d5d765904ba4e43ff299e
EliseyZaf <elisejbaranov46@gmail.com>
2025-05-26 22:35:10 +0400

    to Sergey

--- core/src/main/java/end/team/center/GameCore/Library/ItemType.java
+++ core/src/main/java/end/team/center/GameCore/Library/ItemType.java
@@ -6,7 +6,7 @@ import com.badlogic.gdx.graphics.Texture;
 public enum ItemType {
     exp(new Texture(Gdx.files.internal("UI/GameUI/OtherGameItems/exp.png")), 28, 42),
     accumulator(new Texture(Gdx.files.internal("UI/GameUI/OtherGameItems/accomulator.png")), 48, 56),
-    BreakIron(new Texture(Gdx.files.internal("UI/GameUI/OtherGameItems/_.png")), 5, 5),
+    BreakIron(new Texture(Gdx.files.internal("UI/GameUI/OtherGameItems/stone.png")), 48, 48),
     Bandage(new Texture(Gdx.files.internal("UI/GameUI/OtherGameItems/bandage.png")), 57, 45);
 
     private final Texture texture;
